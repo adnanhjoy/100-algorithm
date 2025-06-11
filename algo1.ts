cc24f5c006a2ab27e467ecc33dfff6ccd45fedd9
Adnan Hossain <engr.adnanhossain@gmail.com>
2025-06-11 11:52:00 +0600

    add question algo1

--- algo1.ts
+++ algo1.ts
@@ -1,3 +1,15 @@
+/*
+Give an array of integers, replace all the occurrences of elementToReplace with subtractionElem.
+
+Example
+For inputArray = [1, 2, 1], elementToReplace = 1 and subtractionElem = 3, the output should be arrayReplace(inputArray, elementToReplace, substrationElem) = [3, 2, 3].
+
+Input/Output
+[time limit] 400ms (js)
+[input] array.integer inputArray
+*/
+
+
 const arrayReplace = (inputArray: number[], elemToReplace: number, substitutionElem: number) => {
     inputArray.forEach((element, index) => {
         if (element === elemToReplace) {
